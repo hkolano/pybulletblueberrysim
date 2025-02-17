9ace6ebe78b52fbd6a2f49bcdea1a60502f6409d
Hannah Kolano <hkolano@comcast.net>
2025-02-17 15:07:05 -0800

    Added some simple pendulum learning code. pendulum.py and simple.urdf or single_pendulum.urdf work to create a single normal pendulum.

--- learning_pybullet/hellopybulletworld.py
+++ learning_pybullet/hellopybulletworld.py
@@ -1,22 +1,38 @@
 import pybullet as p
 import time
+
 import pybullet_data
 
-# Set up the environment
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) 
-p.setGravity(0,0,-10)
-planeID = p.loadURDF("plane.urdf")
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.loadURDF("plane.urdf", [0, 0, -0.25])
+minitaur = p.loadURDF("quadruped/minitaur_single_motor.urdf", useFixedBase=True)
+print(p.getNumJoints(minitaur))
+p.resetDebugVisualizerCamera(cameraDistance=1,
+                             cameraYaw=23.2,
+                             cameraPitch=-6.6,
+                             cameraTargetPosition=[-0.064, .621, -0.2])
+motorJointId = 1
+p.setJointMotorControl2(minitaur, motorJointId, p.VELOCITY_CONTROL, targetVelocity=100000, force=0)
+print(p.getJointInfo(minitaur, motorJointId))
+p.resetJointState(minitaur, motorJointId, targetValue=0, targetVelocity=1)
+angularDampingSlider = p.addUserDebugParameter("angularDamping", 0, 1, 0)
+jointFrictionForceSlider = p.addUserDebugParameter("jointFrictionForce", 0, 0.1, 0)
 
-# Load the robot
-startPos = [0, 0, 1]
-startOrientation = p.getQuaternionFromEuler([0,0,0])
-boxID = p.loadURDF("r2d2.urdf",startPos, startOrientation, useFixedBase = False)
+textId = p.addUserDebugText("jointVelocity=0", [0, 0, -0.2])
+p.setRealTimeSimulation(1)
+while (1):
+  frictionForce = p.readUserDebugParameter(jointFrictionForceSlider)
+  angularDamping = p.readUserDebugParameter(angularDampingSlider)
+  p.setJointMotorControl2(minitaur,
+                          motorJointId,
+                          p.VELOCITY_CONTROL,
+                          targetVelocity=0,
+                          force=frictionForce)
+  p.changeDynamics(minitaur, motorJointId, linearDamping=0, angularDamping=angularDamping)
 
-
-for i in range (10000):
-    p.stepSimulation()
-    time.sleep(1./240.)
-    cubePos,cubeOrn = p.getBasePositionAndOrientation(boxID)
-    print(f'Current Position:{cubePos}', f'Current Orientation: {cubeOrn}')
-p.disconnect()
+  time.sleep(0.01)
+  txt = "jointVelocity=" + str(p.getJointState(minitaur, motorJointId)[1])
+  prevTextId = textId
+  textId = p.addUserDebugText(txt, [0, 0, -0.2])
+  p.removeUserDebugItem(prevTextId)
