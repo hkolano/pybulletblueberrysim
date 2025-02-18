48434185b6bbc38c5b70fb33452e735ba62719ea
Hannah Kolano <hkolano@comcast.net>
2025-02-18 11:54:24 -0800

    Single pendulum and angular damping slider works in simulation

--- learning_pybullet/pendulum.py
+++ learning_pybullet/pendulum.py
@@ -12,7 +12,8 @@ boxId = p.loadURDF("urdf/single_pendulum.urdf", useFixedBase=True)
 
 # get rid of all the default damping forces
 p.changeDynamics(boxId, 1, linearDamping=0, angularDamping=0)
-p.changeDynamics(boxId, 2, linearDamping=0, angularDamping=0)
+
+angularDampingSlider = p.addUserDebugParameter("angularDamping", 0, 1, 0)
 
 # go to the starting position
 p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetPosition=q0, controlMode=p.POSITION_CONTROL)
@@ -21,7 +22,10 @@ for _ in range(1000):
 
 # turn off the motor for the free motion
 p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0)
+p.setRealTimeSimulation(1)
 while True:
+    angularDamping = p.readUserDebugParameter(angularDampingSlider)
+    p.changeDynamics(boxId, 1, angularDamping=angularDamping)
     p.stepSimulation()
     time.sleep(dt)
 p.disconnect()
